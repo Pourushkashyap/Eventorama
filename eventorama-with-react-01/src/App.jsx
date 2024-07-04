a1dab7e1b9a9886de28b240c1f8e833ecd71602d
pourush kahsyap <pourushkashyap06@gmail.com>
2024-07-04 10:34:33 +0530

    add file

---
+++ eventorama-with-react-01/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+<>
+   
+
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ eventorama-with-react-01/src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+
+// document.querySelector(".post").addEventListener("click",() =>{
+//   console.log("pourush")
+
+// }
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <Remove /> */}
+  </React.StrictMode>,
+)
